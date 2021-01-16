83872d2cc2fd21cb8a889e1c5a54801b586b5939
Hong <41907842+jaewon1013@users.noreply.github.com>
2021-01-15 18:48:51 -0600

    Update viewer.js

--- viewer/web/viewer.js
+++ viewer/web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: '.pdf',
+    value: 'information_interaction.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
